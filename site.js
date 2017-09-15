7f38977236e019a34fc348646a67a10d2604031b
Mia Mia <aceline0326@gmail.com>
2017-09-15 13:54:47 -0500

    try this; valid html document

---
+++ site.js
@@ -0,0 +1,22 @@
+var food = {
+	"Dishes": {
+		"name": "Sweet Soy Chicken Wings",
+		"number": "16",
+    "type": "stir-fried",
+    "cook": "Sihan Zhou",
+		"age": "28",
+		"material": [
+		"salt",
+		"soysauce",
+    "Chicken Wings",
+    "sugar"
+		],
+		"phones": {
+			"home": "800-123-4567",
+			"cell": "977-1233-4567"
+		}
+	},
+  $('#result').hide();
+  	$('#button').on("click",function(e){
+  $('#result').show();
+});
